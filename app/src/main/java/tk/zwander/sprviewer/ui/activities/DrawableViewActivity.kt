620c3a2bcdac16053910be8313aec9a1c46475fa
Zachary Wander <zachary.wander@gmail.com>
2019-02-23 23:03:06 -0500

    Export Bitmaps at full resolution

--- app/src/main/java/tk/zwander/sprviewer/ui/activities/DrawableViewActivity.kt
+++ app/src/main/java/tk/zwander/sprviewer/ui/activities/DrawableViewActivity.kt
@@ -1,6 +1,5 @@
 package tk.zwander.sprviewer.ui.activities
 
-import android.app.Activity
 import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
@@ -11,21 +10,27 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 import kotlinx.android.synthetic.main.activity_drawable_view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import tk.zwander.sprviewer.R
 import tk.zwander.sprviewer.util.getAppRes
+import java.io.FileDescriptor
 import java.io.FileOutputStream
 
 class DrawableViewActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_DRAWABLE_NAME = "drawable_name"
 
-        private const val PERM_REQ = 100
         private const val SAVE_REQ = 101
     }
 
-    val drawableId by lazy { intent.getStringExtra(EXTRA_DRAWABLE_NAME) }
+    val drawableName by lazy { intent.getStringExtra(EXTRA_DRAWABLE_NAME) }
+    val drawableId: Int
+        get() = remRes.getIdentifier(drawableName, "drawable", pkg)
     val pkg by lazy { intent.getStringExtra(Intent.EXTRA_PACKAGE_NAME) }
+    val remRes by lazy { getAppRes(pkg) }
 
     private val isViewingAnimatedImage: Boolean
         get() = image.anim != null
@@ -34,64 +39,45 @@ class DrawableViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawable_view)
 
-        if (drawableId.isNullOrBlank() || pkg.isNullOrBlank()) {
+        if (drawableName.isNullOrBlank() || pkg.isNullOrBlank()) {
             finish()
             return
         }
 
-        val res = getAppRes(pkg)
-
         try {
-            val id = res.getIdentifier(drawableId, "drawable", pkg)
-
-            Picasso.Builder(this)
-                .build()
-                .load(
-                    Uri.parse(
-                        "${ContentResolver.SCHEME_ANDROID_RESOURCE}://" +
-                                "$pkg/" +
-                                "${res.getResourceTypeName(id)}/" +
-                                "$id"
-                    )
-                )
-                .into(image)
+            makePicasso(
+                Picasso.Listener { _, _, _ ->
+                    image.setImageDrawable(remRes.getDrawable(drawableId))
+                }
+            ).into(image)
         } catch (e: Exception) {
             Toast.makeText(this, R.string.load_image_error, Toast.LENGTH_SHORT).show()
             finish()
         }
 
         save.setOnClickListener {
-//            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-//                handleSave()
-//            } else {
-//                requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), PERM_REQ)
-//            }
-
-            handleSave()
+            startSave()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            PERM_REQ -> {
-                if (resultCode == Activity.RESULT_OK) handleSave()
-            }
             SAVE_REQ -> {
                 val desc = contentResolver.openFileDescriptor(data!!.data!!, "rw")!!.fileDescriptor
 
-                FileOutputStream(desc).use { output ->
-                    if (isViewingAnimatedImage) {
-                        //TODO something...
-                    } else {
+                GlobalScope.launch {
+                    handleSave(
                         image.drawable.run {
                             if (this is BitmapDrawable) {
-                                bitmap
+                                saveDirect(desc)
+                                return@launch
                             } else {
                                 val ratio = intrinsicHeight.toFloat() / intrinsicWidth.toFloat()
                                 toBitmap(512, (512 * ratio).toInt())
                             }
-                        }.compress(Bitmap.CompressFormat.PNG, 100, output)
-                    }
+                        },
+                        desc
+                    )
                 }
             }
         }
@@ -99,7 +85,7 @@ class DrawableViewActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun handleSave() {
+    private fun startSave() {
         val animated = isViewingAnimatedImage
 
         if (animated) {
@@ -111,8 +97,54 @@ class DrawableViewActivity : AppCompatActivity() {
 
         saveIntent.addCategory(Intent.CATEGORY_OPENABLE)
         saveIntent.type = if (animated) "text/xml" else "images/png"
-        saveIntent.putExtra(Intent.EXTRA_TITLE, "$drawableId.${if (animated) "xml" else "png"}")
+        saveIntent.putExtra(Intent.EXTRA_TITLE, "$drawableName.${if (animated) "xml" else "png"}")
 
         startActivityForResult(saveIntent, SAVE_REQ)
+    }
+
+    private fun handleSave(bitmap: Bitmap, desc: FileDescriptor) {
+        FileOutputStream(desc).use { output ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+        }
+    }
+
+    private fun saveDirect(desc: FileDescriptor) {
+        val res = remRes.openRawResource(drawableId)
+        val file = FileOutputStream(desc)
+
+        val buffer = ByteArray(8192)
+
+        var n: Int
+
+        try {
+            while(true) {
+                n = res.read(buffer)
+
+                if (n <= 0) break
+
+                file.write(buffer, 0, n)
+            }
+        } finally {
+            res.close()
+            file.close()
+        }
+    }
+
+    private fun makePicasso(listener: Picasso.Listener? = null): RequestCreator {
+        return Picasso.Builder(this)
+            .apply {
+                if (listener != null) {
+                    listener(listener)
+                }
+            }
+            .build()
+            .load(
+                Uri.parse(
+                    "${ContentResolver.SCHEME_ANDROID_RESOURCE}://" +
+                            "$pkg/" +
+                            "${remRes.getResourceTypeName(drawableId)}/" +
+                            "$drawableId"
+                )
+            )
     }
 }
